48e4a2388fb7491cd9fdcfa22365523ead94e55f
dima <3277898@gmail.com>
2019-02-25 22:45:28 +0300

    advanced zeros task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+    let obj = {};
+    let mas = [];
+    let c = 2;
+
+    while (base > 1) {
+        if (base % c === 0) {
+            let n = base / c;
+            obj[c] ? obj[c]++ : obj[c]=1;
+            base = n;
+        } else {
+            c++;
+        }
+    }
+
+    for (let p in obj) {
+        if (!obj.hasOwnProperty(p)) { continue; }
+        let r = 0;
+        let num = +p;
+
+        while (num < number) {
+            r+= parseInt(number / num);
+            num = num * p;
+        }
+
+        mas.push(parseInt(r / obj[p]));
+    }
+
+    return Math.min(...mas);
 }
